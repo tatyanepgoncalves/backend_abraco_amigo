2ca49113844d09b97bba099a482e865b2014e57f
tatyanepgoncalves <tatygoncalves715@gmail.com>
2026-04-22 20:03:48 -0300

    feat: create a GET route for getting a location by id

--- src/routes/locaisRoute.js
+++ src/routes/locaisRoute.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { CreateLocationController } from '../controllers/locais/createLocationController.js'
+import { GetLocationByIdController } from '../controllers/locais/getLocationByIdController.js'
 import { isAuthenticated } from '../middlewares/isAuthenticated.js'
 
 export const locaisRoute = Router()
@@ -9,4 +10,11 @@ locaisRoute.post(
   '/locais',
   isAuthenticated,
   new CreateLocationController().handle
+)
+
+// Busca location pelo id com query
+locaisRoute.get(
+  '/locais',
+  isAuthenticated,
+  new GetLocationByIdController().handle
 )
