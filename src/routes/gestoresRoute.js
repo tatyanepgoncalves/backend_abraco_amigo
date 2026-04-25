9ed5f7616bf78df49fdeb4ea6d0202a4c72a2437
tatyanepgoncalves <tatygoncalves715@gmail.com>
2026-04-25 18:53:25 -0300

    feat: create a GET route for getting a gestor by token

--- src/routes/gestoresRoute.js
+++ src/routes/gestoresRoute.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 import { CreateManagerController } from '../controllers/gestores/CreateManagerController.js'
+import { GetManagerDetailController } from '../controllers/gestores/GetManagerDetailController.js'
+import { isAuthenticated } from '../middlewares/isAuthenticated.js'
 import { validateSchema } from '../middlewares/validateSchema.js'
 import { schemaManagers } from '../schema/gestores/index.js'
 
@@ -10,4 +12,11 @@ gestoresRoute.post(
   '/gestores',
   validateSchema(schemaManagers.createManagerSchema),
   new CreateManagerController().handle
+)
+
+// Busca por informações do gestor via token
+gestoresRoute.get(
+  '/gestores/me',
+  isAuthenticated,
+  new GetManagerDetailController().handle
 )
--- src/services/gestores/GetManagerDetailService.js
+++ src/services/gestores/GetManagerDetailService.js
@@ -3,18 +3,17 @@ import { db } from '../../db/connection.js'
 import { schema } from '../../db/schema/index.js'
 import { formatDate, formatPhone } from '../../lib/utils.js'
 
-export class GetUserDetailService {
+export class GetManagerDetailService {
   async execute({ id }) {
     const result = await db
       .select({
-        id: schema.usuarios.id,
-        nome: schema.usuarios.nome,
-        email: schema.usuarios.email,
-        telefone: schema.usuarios.telefone,
-        endereco: schema.usuarios.endereco,
-        userTipo: schema.usuarios.userTipo,
-        criadoEm: schema.usuarios.criadoEm,
-        atualizadoEm: schema.usuarios.atualizadoEm,
+        id: schema.gestor.id,
+        nome: schema.gestor.nome,
+        email: schema.gestor.email,
+        telefone: schema.gestor.telefone,
+        endereco: schema.gestor.endereco,
+        criadoEm: schema.gestor.criadoEm,
+        atualizadoEm: schema.gestor.atualizadoEm,
         locais: {
           nome: schema.locais.nome,
           email: schema.locais.email,
@@ -22,10 +21,10 @@ export class GetUserDetailService {
           endereco: schema.locais.endereco,
           tipoLocal: schema.locais.tipoLocal,
           gestor: {
-            id: schema.usuarios.id,
-            nome: schema.usuarios.nome,
-            telefone: schema.usuarios.telefone,
-            email: schema.usuarios.email,
+            id: schema.gestor.id,
+            nome: schema.gestor.nome,
+            telefone: schema.gestor.telefone,
+            email: schema.gestor.email,
           },
           criadoEm: schema.locais.criadoEm,
           atualizadoEm: schema.locais.atualizadoEm,
@@ -42,16 +41,13 @@ export class GetUserDetailService {
           atualizadoEm: schema.demandas.atualizadoEm,
         },
       })
-      .from(schema.usuarios)
-      .leftJoin(schema.locais, eq(schema.locais.gestorId, schema.usuarios.id))
-      .leftJoin(
-        schema.demandas,
-        eq(schema.demandas.usuarioId, schema.usuarios.id)
-      )
-      .where(eq(schema.usuarios.id, id))
+      .from(schema.gestor)
+      .leftJoin(schema.locais, eq(schema.locais.gestorId, schema.gestor.id))
+      .leftJoin(schema.demandas, eq(schema.demandas.gestorId, schema.gestor.id))
+      .where(eq(schema.gestor.id, id))
 
     if (!result || result.length === 0) {
-      throw new Error('Usuário não encontrado.')
+      throw new Error('Gestor não encontrado.')
     }
 
     const formattedResult = result.map((row) => ({
@@ -60,7 +56,6 @@ export class GetUserDetailService {
       criadoEm: formatDate(row.criadoEm, true),
       atualizadoEm: formatDate(row.atualizadoEm, true),
 
-      // Verificação segura usando optional chaining ou checando o objeto
       locais: row.locais?.id
         ? {
             ...row.locais,
@@ -73,7 +68,6 @@ export class GetUserDetailService {
       demandas: row.demandas?.id
         ? {
             ...row.demandas,
-            telefone: formatPhone(row.demandas.telefone),
             criadoEm: formatDate(row.demandas.criadoEm, true),
             atualizadoEm: formatDate(row.demandas.atualizadoEm, true),
           }
