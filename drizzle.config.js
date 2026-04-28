63f14d6dc5a2cac62c12ca4952b5677722de4dea
Tatyane Gonçalves <158174190+tatyanepgoncalves@users.noreply.github.com>
2026-04-27 21:37:47 -0300

    fix: update schema path in drizzle configuration

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  schema: './src/db/schema/**.js',
+  schema: './src/db/schema/index.js',
   out: './src/db/migrations/',
   dialect: 'postgresql',
   dbCredentials: {
