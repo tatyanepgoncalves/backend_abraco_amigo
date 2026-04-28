de60219c9adcfa17215628943112908623e8d1e0
Tatyane Gonçalves <158174190+tatyanepgoncalves@users.noreply.github.com>
2026-04-28 11:07:27 -0300

    fix: update server log message format

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ import { app } from './src/app.js'
 const PORT = process.env.PORT || 3333
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port http://localhost:${PORT}`)
+  console.log(`Server is running on port: ${PORT}`)
 })
