6ac25df0d58b970783436aabeb126c8a67241208
Tatyane Gonçalves <158174190+tatyanepgoncalves@users.noreply.github.com>
2026-04-28 11:06:02 -0300

    feat: simplify PORT assignment in server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import 'dotenv/config'
 import { app } from './src/app.js'
-import { env } from './src/config/env.js'
 
-const PORT = process.env.PORT || env.PORT || 3333
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port http://localhost:${PORT}`)
