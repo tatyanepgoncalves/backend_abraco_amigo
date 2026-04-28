d46635ab233e52432188247641509ec25d5987c8
Tatyane Gonçalves <158174190+tatyanepgoncalves@users.noreply.github.com>
2026-04-28 10:38:45 -0300

    fix: bind server to all network interfaces

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ import { env } from './src/config/env.js'
 
 const PORT = process.env.PORT || env.PORT || 3333
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port http://localhost:${PORT}`)
 })
