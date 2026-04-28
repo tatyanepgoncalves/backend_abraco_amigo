be7079936afcd1dcadb8a767ce0a9788aadb02f4
Tatyane Gonçalves <158174190+tatyanepgoncalves@users.noreply.github.com>
2026-04-28 18:29:36 -0300

    test: refactor server.js to use express for appTeste

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import 'dotenv/config'
-import { app } from './src/app.js'
+// import { app } from './src/app.js'
+
+import express from 'express'
+const appTeste = express()
 
 const PORT = process.env.PORT || 3333
 
-app.listen(PORT, '0.0.0.0', () => {
+appTeste.get('/', (req, res) => res.send('O servidor subiu!'))
+
+appTeste.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port: ${PORT}`)
 })
