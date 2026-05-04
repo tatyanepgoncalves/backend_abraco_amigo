96643a27e8499a50f5fa173c6245dd409cb29c62
tatyanepgoncalves <tatygoncalves715@gmail.com>
2026-05-04 09:47:09 -0300

    fix: add railway variables

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { app } from './src/app.js'
 
 const PORT = process.env.PORT || 3333
 
+console.log(process.env.DATABASE_URL)
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor online na porta ${PORT}`)
--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,10 @@ import postgres from 'postgres'
 import { env } from '../config/env.js'
 import { schema } from './schema/index.js'
 
-export const pg = postgres(env.DATABASE_URL)
+export const pg = postgres(env.DATABASE_URL, {
+  ssl: 'require',
+})
+
 export const db = drizzle(pg, {
   schema,
   casing: 'camelCase',
